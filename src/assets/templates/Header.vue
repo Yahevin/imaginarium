de393f3618285e04714e64831f6219e2ad35bf0b
ramazan <r.abdullin@codezavod.ru>
2019-11-06 13:57:52 +0300

    base configure webpack

---
+++ src/assets/templates/Header.vue
@@ -0,0 +1,10 @@
+<template>
+	<header>
+		<h2>Alive</h2>
+	</header>
+</template>
+<script>
+	export default {
+	  name: "Header",
+	}
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,74 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/js/main.js',
-
+    polyfill: '@babel/polyfill',
+    app: './src/main.js',
   },
   module: {
     rules: [
       {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        include: path.resolve(__dirname, 'src/assets'),
+      },
+      {
         test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
         loader: 'babel-loader',
+        include: path.resolve(__dirname, 'src/assets'),
       },
       {
         test: /\.scss$/,
-        use: [
-          // process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
+        include: path.resolve(__dirname, 'src/assets/styles'),
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader','sass-loader'],
+        }),
       },
     ],
+  },
+  resolve: {
+    extensions: [ '.js', '.vue','.scss' ],
+    alias: {
+      'vue$': 'vue/dist/vue.runtime.js',
+      '@': path.resolve(__dirname, 'src'),
+    }
   },
 
   devServer: {
     contentBase: './dist',
+    compress: true,
+    historyApiFallback: true,
+    hot: true,
+    open: true,
+    overlay: true,
+    port: 8000,
+    stats: {
+      normal: true
+    }
   },
 
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({template: './src/index.html'})
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({template: './src/assets/index.html'}),
+    new ExtractTextPlugin('style.css'),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 8000,
+    })
   ],
 
   output: {
-    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: './',
+    publicPath: '/',
+    filename: 'js/[name].bundle.js',
+    chunkFilename: 'js/[id].chunk.js'
   },
 };
