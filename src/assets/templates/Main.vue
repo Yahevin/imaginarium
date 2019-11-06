beb24595bb428652fe9af5a2ca7338ff729fe44c
ramazan <r.abdullin@codezavod.ru>
2019-11-06 14:41:00 +0300

    install Vue

---
+++ src/assets/templates/Main.vue
@@ -0,0 +1,8 @@
+<template>
+	<h1 style="margin: 200px auto 0 auto; display: block; width: 100px;">Hello my World</h1>
+</template>
+<script>
+	export default {
+	  name: "Main",
+	}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
-import './assets/styles/main.scss'
+import './assets/styles/main.scss';
+import Vue from "vue";
+import Main from "./assets/templates/Main.vue";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(Main)
+}).$mount("#app");
 
 console.log('hello');
