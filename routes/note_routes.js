3817c3d275961e6d827a9a5717358fea1d4e3331
ramazan <r.abdullin@codezavod.ru>
2019-11-26 19:16:17 +0300

    add 'set game action' back request

--- routes/note_routes.js
+++ routes/note_routes.js
@@ -149,6 +149,18 @@ module.exports = async function(app, db) {
 	});
 	
 	
+	app.post('/set-action', async (req, res) => {
+		let roomId = req.body.room_id,
+				action = req.body.action;
+		
+		let setActionReq = db.format(sql.usw, ['room', 'game_action', action, 'id', roomId]);
+			db.query(setActionReq, function (err, results) {
+				if (err) throw err;
+				res.json({success: true});
+			});
+	});
+	
+	
 	app.post('/card-main', async (req, res) => {
 		let handCardId = req.body.id,
 				cardId = req.body.card_id,
