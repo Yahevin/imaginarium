71909e06eea9ed8a2d3f1bc1b7b1c4a96b1f3c06
ramazan <r.abdullin@codezavod.ru>
2019-12-09 16:55:19 +0300

    add 'preview' template

---
+++ src/assets/templates/Preview.vue
@@ -0,0 +1,23 @@
+<template>
+	<section class="preview">
+			<article class="preview__sky hue-rotate"></article>
+			<article class="preview__mountains mountain-grid hue-rotate">
+				<img class="mountain-grid__item" src="@/assets/img/mountain-left.png">
+				<img class="mountain-grid__item" src="@/assets/img/mountain-center.png">
+				<img class="mountain-grid__item" src="@/assets/img/mountain-right.png">
+			</article>
+			<article class="preview__abyss">
+				<img src="@/assets/img/abyss.png">
+			</article>
+	</section>
+</template>
+
+<script>
+	export default {
+		name: "Preview"
+	}
+</script>
+
+<style scoped>
+
+</style>
