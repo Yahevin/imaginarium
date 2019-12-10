35343ac2466947a351eda3e1bdd093a5541ac68c
ramazan <r.abdullin@codezavod.ru>
2019-12-10 10:51:52 +0300

    add animation to preview

--- src/assets/templates/Preview.vue
+++ src/assets/templates/Preview.vue
@@ -1,20 +1,35 @@
 <template>
 	<section class="preview">
-			<article class="preview__sky hue-rotate"></article>
-			<article class="preview__mountains mountain-grid hue-rotate">
-				<img class="mountain-grid__item" src="@/assets/img/mountain-left.png">
-				<img class="mountain-grid__item" src="@/assets/img/mountain-center.png">
-				<img class="mountain-grid__item" src="@/assets/img/mountain-right.png">
-			</article>
-			<article class="preview__abyss">
-				<img src="@/assets/img/abyss.png">
-			</article>
+		<transition name="preview-first">
+			<div v-show="showFirst"
+			     @click="showFirst = false"
+			     class="preview__section">
+				<article class="preview__mountains mountain-grid hue-rotate">
+					<img class="mountain-grid__item" src="@/assets/img/mountain-left.png">
+					<img class="mountain-grid__item" src="@/assets/img/mountain-center.png">
+					<img class="mountain-grid__item" src="@/assets/img/mountain-right.png">
+				</article>
+				<article class="preview__abyss">
+					<img src="@/assets/img/abyss.png">
+				</article>
+			</div>
+		</transition>
+			<div v-show="!showFirst"
+					class="preview__section">
+					<article class="preview__dark darkness">
+					</article>
+			</div>
 	</section>
 </template>
 
 <script>
 	export default {
-		name: "Preview"
+		name: "Preview",
+		data() {
+			return {
+				showFirst: true,
+			}
+		},
 	}
 </script>
 
