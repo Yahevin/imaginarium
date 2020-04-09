e3e556d1cec08164c701b0dee4a42575223c1174
ramazan <root@codezavod.ru>
2020-04-09 14:33:39 +0300

    Refactor login request

--- routes/models/Party.js
+++ routes/models/Party.js
@@ -1,47 +1,112 @@
 const sql = require('../mixins/sqlCommands');
 
 module.exports = {
-	getUsersIn: async function (app, db, roomId) {
-		return new Promise(async (resolve, reject) => {
-			let format = db.format(sql.sfw, ['user__room', 'room_id', roomId]);
-			
-			return db.query(format, function (err, results) {
-				if (err) reject(err);
-				return resolve(results);
-			});
+	getUsersIn: function (app, db, room_id) {
+		return new Promise((resolve, reject) => {
+      try {
+        let format = db.format(sql.sfw, ['user__room', 'room_id', room_id]);
+
+        return db.query(format, function (err, results) {
+          if (err) return reject(err);
+          return resolve(results);
+        });
+      }
+      catch (error) {
+        return reject(error);
+      }
 		}).catch((error) => {
-			console.log(error);
-			return [];
+			throw {desc: 'Function failed: getUsersIn', detail: error};
 		})
 	},
-	getUsersIdList: async function (app, db, roomId) {
-		return new Promise(async (resolve, reject) => {
-			let format = db.format(sql.sfw, ['user__room', 'room_id', roomId]);
-			
-			return db.query(format, function (err, results) {
-				if (err) reject(err);
-				
-				let users = results.map((item) => {
-					return item.user_id;
-				});
-				return resolve(users);
-			});
+	getUsersIdList: function (app, db, room_id) {
+		return new Promise((resolve, reject) => {
+      try {
+        let format = db.format(sql.sfw, ['user__room', 'room_id', room_id]);
+
+        return db.query(format, function (err, results) {
+          if (err) return reject(err);
+          if(results.length > 0) {
+            let users = results.map ((item) => {
+              return item.user_id;
+            });
+            return resolve (users);
+          } else {
+            return resolve ([]);
+          }
+			  });
+      }
+      catch (error) {
+        return reject(error);
+      }
 		}).catch((error) => {
-			console.log(error);
-			return [];
+      throw {desc: 'Function failed: getUsersIdList', detail: error};
 		})
 	},
-	getPlayersCount: async function (app, db, room_id) {
-		return new Promise(async (resolve, reject) => {
+	getPlayersCount: function (app, db, room_id) {
+		return new Promise((resolve, reject) => {
 			let format = db.format(sql.sfw, ['room', 'id', room_id]);
 			
 			return db.query(format, function (err, results) {
 				if (err) reject(err);
-				return resolve(results[0].player_count);
+				if(results.length > 0 && results[0].hasOwnProperty('player_count')) {
+          return resolve(results[0].player_count);
+        } else {
+          reject('room_id is incorrect');
+        }
 			});
 		}).catch((error) => {
-			console.log(error);
-			return 0;
+      throw {desc: 'Function failed: getPlayersCount', detail: error};
 		})
 	},
+  exist: function (app, db, room_id) {
+    return new Promise((resolve, reject) => {
+      try {
+        let format = db.format(sql.sfw, ['room', 'id', room_id]);
+
+        return db.query(format, function (err, results) {
+          if (err) return reject(err);
+          return resolve(results.length > 0);
+        });
+      }
+      catch (error) {
+        return reject(error);
+      }
+    }).catch((error) => {
+      throw {desc: 'Function failed: Party.exist', detail: error};
+    })
+  },
+  addPlayer: function (app, db, room_id, user_id) {
+    return new Promise((resolve, reject) => {
+      try {
+        let format = db.format(sql.ii2, ['user__room', 'room_id', 'user_id', room_id, user_id]);
+
+        return db.query(format, function (err, results) {
+          if (err) return reject(err);
+          return resolve(results);
+        });
+      }
+      catch (error) {
+        return reject(error);
+      }
+    }).catch((error) => {
+      throw {desc: 'Function failed: addPlayer', detail: error};
+    })
+  },
+  countUpdate: function (app, db, new_count, room_id) {
+    return new Promise((resolve, reject) => {
+      try {
+        let format = db.format(sql.usw, ['room', 'player_count', new_count, 'id', room_id]);
+
+        return db.query(format, function (err, results) {
+          if (err) return reject(err);
+          return resolve();
+        });
+      }
+      catch (error) {
+        return reject(error);
+      }
+    }).catch((error) => {
+      throw {desc: 'Function failed: addPlayer', detail: error};
+    })
+  },
 };
--- routes/models/User.js
+++ routes/models/User.js
@@ -1,17 +1,66 @@
 const sql = require('../mixins/sqlCommands');
 
 module.exports = {
-	gameMaster: async function (app, db, user_id) {
+  create: function (app, db, nick_name) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let format = db.format(sql.ii3, ['users', 'nick_name','game_master','player_style', nick_name, false, null]);
+        return db.query(format, function (err, results) {
+          if (err) return reject(err);
+          return resolve (results.insertId);
+        });
+      }
+      catch (error) {
+        return reject(error);
+      }
+    }).catch((error) => {
+      throw {desc: 'Function failed: User.create', detail: error};
+    })
+  },
+	gameMaster: function (app, db, user_id) {
 		return new Promise(async (resolve, reject) => {
-			let format = db.format(sql.sfw, ['users', 'id', user_id]);
-			
-			return db.query(format, function (err, results) {
-				if (err) reject(err);
-				return resolve(results[0].game_master);
-			});
+      try {
+        let format = db.format(sql.sfw, ['users', 'id', user_id]);
+
+        return db.query(format, function (err, results) {
+          if (err) reject(err);
+          if (results.length > 0) {
+            return resolve (results[0].game_master);
+          } else {
+            reject('Did not found such user');
+          }
+        });
+      }
+      catch (error) {
+        return reject(error);
+      }
 		}).catch((error) => {
-			console.log(error);
-			return false;
+      throw {desc: 'Function failed: gameMaster', detail: error};
 		})
 	},
+  find: function (app, db, nick_name) {
+    return new Promise((resolve, reject) => {
+      try {
+        let format = db.format(sql.sfw, ['users', 'nick_name', nick_name]);
+        return db.query(format, function (err, results) {
+          if (err) reject(err);
+          if(results.length > 0) {
+            return resolve({
+              data: results[0],
+              exist: true,
+            });
+          } else {
+            return resolve({
+              exist: false,
+            });
+          }
+        });
+      }
+      catch (error) {
+        return reject(error);
+      }
+    }).catch((error) => {
+      throw {desc: 'Function failed: User.find', detail: error};
+    })
+  },
 };
