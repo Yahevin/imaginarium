cbf2aea65b36189615bc0e699c2da37db426b5fb
ramazan <root@codezavod.ru>
2020-04-09 19:51:05 +0300

    Refactor table clear logic

--- routes/models/Table.js
+++ routes/models/Table.js
@@ -1,7 +1,7 @@
 const sql = require('../mixins/sqlCommands');
 
 module.exports = {
-	putCard: async function (app, db, img_url, card_id, is_main) {
+	putCard: function (app, db, img_url, card_id, is_main) {
 		return new Promise(async (resolve, reject) => {
 			let format = db.format(sql.ii3, ['cards_on_table', 'img_url', 'card_id', 'is_main', img_url, card_id, is_main]);
 			db.query(format, function (err, results) {
@@ -17,7 +17,7 @@ module.exports = {
 		await this.noteToUser.apply(arguments);
 		await this.noteToRoom.apply(arguments);
 	},
-	noteToUser: async function (app, db, user_id, room_id, table_card_id, card_id, is_main) {
+	noteToUser: function (app, db, user_id, room_id, table_card_id, card_id, is_main) {
 		return new Promise(async (resolve, reject) => {
 			let format = db.format(sql.ii3,
 				['user__table', 'user_id', 'table_card_id', 'is_main', user_id, table_card_id, is_main]);
@@ -32,7 +32,7 @@ module.exports = {
 			return false;
 		})
 	},
-	noteToRoom: async function (app, db, user_id, room_id, table_card_id, card_id) {
+	noteToRoom: function (app, db, user_id, room_id, table_card_id, card_id) {
 		return new Promise(async (resolve, reject) => {
 			let format = db.format(sql.ii3,
 				['room__table', 'room_id', 'table_card_id', 'card_id', room_id, table_card_id, card_id]);
@@ -46,7 +46,7 @@ module.exports = {
 			return false;
 		})
 	},
-	getCardsCount: async function (app, db, room_id) {
+	getCardsCount: function (app, db, room_id) {
 		return new Promise(async (resolve, reject) => {
 			let format = db.format(sql.sfw, ['room__table', 'room_id', room_id]);
 			
@@ -59,4 +59,72 @@ module.exports = {
 			return false;
 		})
 	},
+  getItemsIdList: function (app, db, room_id) {
+    return new Promise((resolve, reject) => {
+      try {
+        let format = db.format(sql.sfw, ['room__table', 'room_id', room_id]);
+        return db.query(format, function (err, results) {
+          if (err) return reject(err);
+          if(results.length > 0) {
+            return resolve (
+              results.map((item) => {
+                if (item.hasOwnProperty ('table_card_id')) {
+                  return item.table_card_id;
+                }
+              })
+            );
+          } else {
+            return reject('result is empty');
+          }
+        });
+      }
+      catch (error) {
+        return reject(error);
+      }
+    }).catch((error) => {
+      throw {desc: 'Function failed: Table.getItemsIdList', detail: error};
+    })
+  },
+  getCardsIdList: function (app, db, table_items_id_list) {
+    return new Promise((resolve, reject) => {
+      try {
+        let format = db.format(sql.sfwi, ['cards_on_table', 'id', table_items_id_list]);
+        return db.query(format, function (err, results) {
+          if (err) return reject(err);
+          if(results.length > 0) {
+            return resolve (
+              results.map((item) => {
+                if (item.hasOwnProperty('card_id')) {
+                  return item.card_id
+                }
+              })
+            );
+          } else {
+            return reject('result is empty');
+          }
+        });
+      }
+      catch (error) {
+        return reject(error);
+      }
+    }).catch((error) => {
+      throw {desc: 'Function failed: Table.getCardsIdList', detail: error};
+    })
+  },
+  clear: function (app, db, table_id_list) {
+    return new Promise((resolve, reject) => {
+      try {
+        let format = db.format(sql.dfwi, ['cards_on_table', 'id', table_id_list]);
+        return db.query(format, function (err, results) {
+          if (err) return reject(err);
+          return resolve();
+        });
+      }
+      catch (error) {
+        return reject(error);
+      }
+    }).catch((error) => {
+      throw {desc: 'Function failed: Table.clear', detail: error};
+    })
+  }
 };
--- routes/router/table-clear.js
+++ routes/router/table-clear.js
@@ -1,87 +1,27 @@
-const sql = require('../mixins/sqlCommands');
-
+const Distribution = require('../models/Distribution');
+const Table = require('../models/Table');
 
 module.exports = function(app, db) {
 	app.post('/table-clear', async (req, res) => {
-		let roomId = req.body.room_id,
-			tableCards = [],
-			cardsId = [],
-			distribution = {};
-		
-		function getTableCards(resolve) {
-			let getTableCardsReq = db.format(sql.sfw, ['room__table', 'room_id', roomId]);
-			db.query(getTableCardsReq, function (err, results) {
-				if (err) throw err;
-				tableCards = results.map((item, index) => {
-					if (item.hasOwnProperty('table_card_id')) {
-						return item.table_card_id
-					}
-				});
-				cardsId = results.map((item, index) => {
-					if (item.hasOwnProperty('card_id')) {
-						return item.card_id
-					}
-				});
-				return resolve();
-			});
-		}
-		
-		function getDistribution(resolve) {
-			let getDistributionReq = db.format(sql.sfw,
-				['distribution', 'room_id', roomId]);
-			db.query(getDistributionReq, function (err, results) {
-				if (err) throw err;
-				distribution = results[0];
-				return resolve();
-			});
-		}
-		
-		function moveToBasket(resolve) {
-			if (cardsId.length > 0) {
-				cardsId.forEach((currentId, index) => {
-					let moveToBasketReq = db.format(sql.ii2,
-						['in_basket', 'distribution_id', 'card_id', distribution.id, currentId]);
-					db.query(moveToBasketReq, function (err, results) {
-						if (err) throw err;
-						if (index >= (cardsId.length - 1)) {
-							return resolve();
-						}
-					});
-				});
-			} else {
-				resolve();
-			}
-		}
-		
-		function cleanTableCards() {
-			if (tableCards.length > 0) {
-				tableCards.forEach((currentId, index) => {
-					let cleanTableCardsReq = db.format(sql.dfw, ['cards_on_table', 'id', currentId]);
-					db.query(cleanTableCardsReq, function (err, results) {
-						if (err) throw err;
-						if (index >= (tableCards.length - 1)) {
-							res.json({success: true});
-						}
-					});
-				});
-			} else {
-				res.json({success: true});
-			}
-		}
-		
-		new Promise(resolve => {
-			getTableCards(resolve)
-		}).then(() => {
-			new Promise(resolve => {
-				getDistribution(resolve);
-			}).then(() => {
-				new Promise(resolve => {
-					moveToBasket(resolve);
-				}).then(() => {
-					cleanTableCards()
-				});
-			})
-		})
-		
+		const room_id = req.body.room_id;
+
+    try {
+      const table_items_id_list = await Table.getItemsIdList(app, db, room_id);
+      const cards_id_list       = await Table.getCardsIdList(app, db, table_items_id_list);
+      const distribution        = await Distribution.getSelf(app, db, room_id);
+
+      await Distribution.moveToBasket(app, db, distribution.id, cards_id_list);
+      await Table.clear(app, db, table_items_id_list);
+
+      return res.json({
+        success: true,
+      });
+    }
+    catch(error) {
+      return res.json({
+        success: false,
+        error: error,
+      });
+    }
 	});
 };
