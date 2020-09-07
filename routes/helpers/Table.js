e5af23b983d7d4fbc7a201b1efd5dc8eaf50f075
Yahevin <yahevin15@gmail.com>
2020-09-07 19:49:50 +0300

    fix Table helper

--- routes/helpers/Table.js
+++ routes/helpers/Table.js
@@ -5,39 +5,33 @@ const dbQuery = require('../mixins/dbQuery');
 
 module.exports = {
 	putCard: async function (app, db, card_id) {
-    const format = db.format(sql.usw, ['cards', 'status', cardStatus.table, 'card_id', card_id]);
+    const format = db.format(sql.usw, ['card', 'status', cardStatus.table, 'card_id', card_id]);
     await dbQuery(format,db);
 
     return {success: true};
 	},
-  getCard: async function (app, db, user_id, room_id) {
-    const format = db.format(sql.sfwww, ['user__table', 'user_id', user_id, 'room_id', room_id, 'status', cardStatus.table]);
+  getCard: async function (app, db, player_id) {
+    const format = db.format(sql.sfww, ['card', 'player_id', player_id, 'status', cardStatus.table]);
     const results = await dbQuery(format,db);
 
     if (isNotEmpty(results)) {
       return results[0];
     } else {
-      throw ('There is no users card on the table');
+      throw ('There is no user`s card on the table');
     }
   },
-  getCards: async function (app, db, room_id) {
-    const format = db.format(sql.sfww,  ['cards', 'room_id', room_id, 'status', cardStatus.table]);
+  getCardsList: async function (app, db, room_id) {
+    const format = db.format(sql.sfww,  ['card', 'room_id', room_id, 'status', cardStatus.table]);
     const results = await dbQuery(format,db);
 
     if (isNotEmpty(results)) {
-      return results[0];
+      return results;
     } else {
       throw ('There is no such cards on the table');
     }
   },
-  getCardsCount: async function (app, db, room_id) {
-    const format = db.format (sql.sfww, ['cards', 'room_id', room_id, 'status', cardStatus.table]);
-    const results = await dbQuery(format,db);
-
-    return results.length;
-  },
   clear: async function (app, db, room_id, basket_id) {
-    const format = db.format(sql.ussww, ['cards', 'status', cardStatus.basket, 'basket_id', basket_id, 'room_id', room_id, 'status', cardStatus.table]);
+    const format = db.format(sql.ussww, ['card', 'status', cardStatus.basket, 'basket_id', basket_id, 'room_id', room_id, 'status', cardStatus.table]);
     await dbQuery(format,db);
 
     return {success: true};
