2c033044702ed88854a8fcbb330b2c0b64cc1341
Yahevin <yahevin15@gmail.com>
2020-09-08 16:22:53 +0300

    Start Auth page

---
+++ src/pages/Auth/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+
+import PageSize from "@/styled/PageSize";
+import Flex__c_sb from "@/styled/flex/Flex__c_sb";
+
+import ButtonTheme from "@/constants/ButtonTheme";
+
+import Button from "@/components/Button";
+
+const AuthBox = styled.main`
+  ${PageSize};
+  ${Flex__c_sb};
+`;
+
+function Auth() {
+
+    const enter = () => {
+
+    };
+
+    const registration = () => {
+
+    };
+
+    return (
+        <AuthBox>
+            <h1>
+                Imaginarium
+            </h1>
+
+            <div>
+                <Button callback={enter}
+                        theme={ButtonTheme.light}
+                        size={'100%'} >
+                    Войти
+                </Button>
+
+                <Button callback={registration}
+                        theme={ButtonTheme.dark}
+                        size={'100%'} >
+                    Создать аккаунт
+                </Button>
+            </div>
+        </AuthBox>
+    )
+}
+
+export default Auth;
