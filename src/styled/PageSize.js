c7df24e79fa11d98ff08f7e3763b0ef7246bcb11
Yahevin <yahevin15@gmail.com>
2020-09-09 19:57:38 +0300

    update some styles

--- src/styled/PageSize.js
+++ src/styled/PageSize.js
@@ -3,7 +3,7 @@ import mediaQuery from "@/constants/mediaQuery";
 
 const PageSize = css`
   max-width: 1300px;
-  padding: 20px 40px;
+  padding: 60px 40px;
   ${mediaQuery.phone} {
     padding: 20px 10px;
   }
