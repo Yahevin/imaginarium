5156ba26567377c58d0982f9fa60f6e7fb7c04a9
Yahevin <yahevin15@gmail.com>
2020-09-10 16:22:17 +0300

    add reducer for User`s global state

---
+++ src/store/user/reducer.ts
@@ -0,0 +1,27 @@
+import {SET_USER_ID} from "@/store/actions";
+import {SET_USER_SCORE} from "@/store/actions";
+import userState from "@/store/user/state";
+import {UserActionTypes} from "@/store/user/action";
+import IUserState from "@/interfaces/IUserState";
+
+function userReducer(state = userState, action: UserActionTypes) : IUserState {
+    switch (action.type) {
+        case SET_USER_ID: {
+            return {
+                ...state,
+                user_id: action.payload
+            }
+        }
+        case SET_USER_SCORE: {
+            return {
+                ...state,
+                score: action.payload
+            }
+        }
+        default: {
+            return state;
+        }
+    }
+}
+
+export default userReducer;
---
+++ src/store/user/state.ts
@@ -0,0 +1,8 @@
+import IUserState from "@/interfaces/IUserState";
+
+const userState: IUserState = {
+    user_id: null,
+    score: null,
+};
+
+export default userState;
