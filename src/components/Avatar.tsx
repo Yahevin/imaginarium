0970cff1664a532e32e949c1a2e58dfd1cc3f126
Yahevin <yahevin15@gmail.com>
2020-09-11 17:32:51 +0300

    create Avatar component

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,88 @@
+import React, {useCallback, useEffect, useMemo, useRef} from "react";
+import styled, {css} from "styled-components";
+import getRandomColor from "@/helpers/getRandomColor";
+import COLOR from "@/constants/Color";
+
+const Box = styled.div`
+    flex: 1 1 80px;
+    height: auto;
+    max-width: 100px;
+    min-width: 60px;
+`;
+const SubBox = styled.div`
+    width: 100%;
+    height: 100%;
+    padding-bottom: 100%;
+    position: relative;
+`;
+const Round = css`
+    position: absolute;
+    top: 0;
+    text-transform: uppercase;
+    text-align: center;
+    border-radius: 50%;
+`;
+const Face = styled.div`
+    ${Round};
+    width: 100%;
+    height: 100%;
+    left: 0;
+    padding: 16% 16%;
+    background: ${getRandomColor()};
+`;
+const Level = styled.div`
+    ${Round};
+    width: 24px;
+    height: 24px;
+    position: absolute;
+    right: 0;
+    font-size: 12px;
+    line-height: 24px;
+    background: ${COLOR.active};
+`;
+
+interface IAvatar {
+    nick_name: string,
+    score: number,
+}
+
+function Avatar({nick_name, score}: IAvatar) {
+    const userName = useMemo(() => (nick_name[0] + nick_name[nick_name.length - 1]),
+        [nick_name]);
+    const userLevel = useMemo(() => (Math.floor(score / 100)),
+        [score]);
+
+    const $face = useRef(null);
+
+    const setFontSize = useCallback(() => {
+        if ($face.current === null) return;
+
+        const height = $face.current.offsetHeight;
+        $face.current.style.setProperty('font-size', (height * 0.55 + 'px'));
+        $face.current.style.setProperty('line-height', (height * 0.7 + 'px'));
+    }, [$face]);
+
+    useEffect(() => {
+        setFontSize();
+        window.addEventListener('resize', setFontSize);
+
+        return () => {
+            window.removeEventListener('resize', setFontSize);
+        }
+    });
+
+    return (
+        <Box>
+            <SubBox>
+                <Face ref={$face}>
+                    {userName}
+                </Face>
+                <Level>
+                    {userLevel}
+                </Level>
+            </SubBox>
+        </Box>
+    )
+}
+
+export default Avatar;
--- src/constants/Color.ts
+++ src/constants/Color.ts
@@ -1,9 +1,13 @@
 const COLOR = {
-    transparent: 'transparent',
+    red: '#f7786b',
     white: '#f0efef',
     slate: '#3e4444',
     green: '#4CAF50',
-    red: '#f7786b',
+    active: '#666666',
+    passive: '#C4C4C4',
+    dark_bg: '#EEEEEE',
+    light_bg: '#f0efef',
+    transparent: 'transparent',
 };
 
 export default COLOR;
