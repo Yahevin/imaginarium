831a89804d585f47b60cfdaeed99c3e426db128f
Yahevin <yahevin15@gmail.com>
2020-09-14 16:06:38 +0300

    fix room-creating api

--- routes/router/party-create.js
+++ routes/router/party-create.js
@@ -1,24 +1,19 @@
 const gameSt = require('../mixins/gameStatus');
 const Party = require('../helpers/Party');
 const Guess = require('../helpers/Guess');
-const User = require('../helpers/User');
 
 module.exports = function(app, db) {
 	app.post('/party-create', async (req, res) => {
-		let nickName = req.body.nickName;
+		const user_id = req.body.user_id;
 
     try {
       const room_id = await Party.create(app, db);
-      const user_id = await User.create(app, db, nickName, true);
       await Party.addPlayer(app, db, room_id, user_id);
-      await Guess.setQuestionField(app, db, room_id);
+      await Guess.createQuestion(app, db, room_id);
 
       return res.json({
         success: true,
         room_id: room_id,
-        user_id: user_id,
-        nick_name: nickName,
-        player_style: null,
         game_master: true,
         game_action: gameSt.prepare,
       })
