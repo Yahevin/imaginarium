5912eb23c786c3a16a0e96d58b605c3390536807
Yahevin <Yahevin15@gmail.com>
2020-09-16 14:41:08 +0300

    add WebSocket connection

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const webpack = require('webpack');
+const http = require('http');
+const WebSocket = require('ws');
+const SocketController = require('./routes/ws');
 const bodyParser = require('body-parser');
 const config = require('./webpack.config.js');
 const webpackDevMiddleware = require('webpack-dev-middleware');
@@ -19,7 +22,24 @@ app.use(express.json());
 
 require('./routes/db/mydb');
 require('./routes')(app, db);
+require('./routes/ws')(app, db);
 
-app.listen(port, ()=>{
-  console.log(`My app listening on port ${port}`);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+wss.on('connection', (ws) => {
+
+  //connection is up, let's add a simple simple event
+  ws.on('message', async (message) => {
+    const resolution = await SocketController;
+    //log the received message and send it back to the client
+    console.log('received: %s', message);
+    ws.send(`Hello, here server answer -> ${resolution}`);
+  });
+
+  //send immediatly a feedback to the incoming connection
+  ws.send('Hi there, I am a WebSocket server');
+});
+
+server.listen(port, ()=>{
+  console.log(`My app listening on port ${server.address().port}`);
 });
