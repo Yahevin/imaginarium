9d3aaf5bd9ab6ae58e5763605edb3ccd83a12249
Yahevin <Yahevin15@gmail.com>
2020-10-12 10:53:53 +0300

    move interfaces to packages

---
+++ packages/interfaces/index.ts
@@ -0,0 +1,6 @@
+export * from "./parts/ICard";
+export * from "./parts/IPlayer";
+export * from "./parts/IButton";
+export * from "./parts/IPerson";
+export * from "./parts/IThinButton";
+export * from "./parts/InputHandler";
