2307180846b398bfa2ce90ef55353a5b731af643
Yahevin <Yahevin15@gmail.com>
2020-11-02 20:15:55 +0300

    Create util to get random party name

---
+++ server/mixins/getRandomPartyName.js
@@ -0,0 +1,11 @@
+function getRandom(array) {
+  return array[Math.round(array.length * Math.random())]
+}
+
+module.exports = function getRandomPartyName() {
+  const colors = ['white', 'yellow', 'green', 'blue', 'orange', 'lemon', 'red', 'black'];
+  const group = ['fleet', 'party', 'band', 'army', 'bunch', 'squad', 'circle'];
+  const char = ['madness', 'glory', 'death', 'strength', 'fury', 'imagination'];
+
+  return `The ${getRandom(colors)} ${getRandom(group)} of ${getRandom(char)}`;
+};
