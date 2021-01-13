a5dba032a4aa222a4a2d7f8ed812e7781b7729c8
Yahevin <Yahevin15@gmail.com>
2021-01-13 20:03:59 +0300

    Fix storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,7 +11,7 @@ module.exports = {
   webpackFinal: async (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "../src"),
     };
     config.resolve.extensions.push(".ts", ".tsx");
     return config;
