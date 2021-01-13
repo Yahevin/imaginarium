6459bdcd8c690631c28d9912e03ed929e71e118d
Yahevin <Yahevin15@gmail.com>
2021-01-13 17:15:17 +0300

    FIX STORYBOOK TESTS!! FINALLY

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,19 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../**/*.stories.@(jsx|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
-}
+  ],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+    };
+    config.resolve.extensions.push(".ts", ".tsx");
+    return config;
+  },
+};
--- src/storyshots.test.js
+++
@@ -1,5 +0,0 @@
-import initStoryshots from '@storybook/addon-storyshots';
-import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
-
-// initStoryshots({ /* configuration options */ });
-initStoryshots({ suite: 'Puppeteer storyshots', test: imageSnapshot() });
