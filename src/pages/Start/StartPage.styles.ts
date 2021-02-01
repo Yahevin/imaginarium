21a44ad6508604c3361af1d652f361d743266e16
Yahevin <Yahevin15@gmail.com>
2021-02-01 13:21:14 +0300

    Update Start page

---
+++ src/pages/Start/StartPage.styles.ts
@@ -0,0 +1,40 @@
+import styled, { css } from 'styled-components';
+import { COLOR } from '@my-app/constants';
+
+export const SideTitle = styled.div`
+  margin: 0 0 32px 0;
+  color: ${COLOR.black};
+  transition: color 0.5s 0.3s ease;
+  text-transform: uppercase;
+  opacity: 0.6;
+  font-size: 24px;
+  font-weight: 700;
+`;
+// TODO add typography
+
+export const PageBtn = css`
+  position: absolute;
+  top: 0;
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  cursor: none;
+
+  &:hover {
+    & > ${SideTitle} {
+      color: ${COLOR.green};
+    }
+  }
+`;
+
+export const LeftSide = styled.div`
+  ${PageBtn};
+  left: 0;
+`;
+
+export const RightSide = styled.div`
+  ${PageBtn};
+  right: 0;
+`;
