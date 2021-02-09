8aa2791d995d7892fdd72ef08a3aa96fb547f87e
Yahevin <Yahevin15@gmail.com>
2021-02-09 14:01:29 +0300

    Use Header component in game pages

--- src/pages/Lobby/LobbyPage.tsx
+++ src/pages/Lobby/LobbyPage.tsx
@@ -1,12 +1,9 @@
-import React, { useCallback, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-import { PartyAction } from '@/store/party/action';
 import { TStore } from '@/store/reducer';
-import SocketAction from '@/web-socket/action';
-
-import { BUTTON_THEME, MIN_PLAYERS_COUNT, PAGES } from '@my-app/constants';
+import { MIN_PLAYERS_COUNT, PAGES } from '@my-app/constants';
 import { Menu, Menu__item } from '@/styled/Menu';
 
 import updateQuestion from '@/api-actions/updateQuestion';
@@ -14,12 +11,9 @@ import updateTable from '@/api-actions/updateTable';
 import updateHand from '@/api-actions/updateHand';
 
 import { PlayersGrid } from '@/components/PlayersGrid/PlayersGrid';
-import { Button } from '@/components/Button/Button';
-import Spacer from '@/styled/Spacer';
-import { FlexRowBox } from '@/styled/Flex';
+import { HeaderInGame } from '@/components/HeaderInGame/HeaderInGame';
 
 export const LobbyPage = () => {
-  const dispatch = useDispatch();
   const history = useHistory();
   const players = useSelector((store: TStore) => store.partyReducer.players);
 
@@ -35,23 +29,12 @@ export const LobbyPage = () => {
     updateQuestion();
   }, []);
 
-  const leaveParty = useCallback(() => {
-    SocketAction.leave();
-
-    dispatch(PartyAction.leave());
-    history.replace(PAGES.MAIN);
-  }, [dispatch, history]);
-
   return (
     <Menu>
       <Menu__item>
-        <FlexRowBox>
+        <HeaderInGame>
           <h4>Начало игры</h4>
-          <Spacer />
-          <Button callback={leaveParty} theme={BUTTON_THEME.DARK}>
-            Покинуть игру
-          </Button>
-        </FlexRowBox>
+        </HeaderInGame>
       </Menu__item>
 
       <Menu__item>
--- src/pages/Scores/ScoresPage.tsx
+++ src/pages/Scores/ScoresPage.tsx
@@ -5,20 +5,22 @@ import { useHistory } from 'react-router-dom';
 import SocketAction from '@/web-socket/action';
 import { TStore } from '@/store/reducer';
 
-import { BUTTON_THEME, GAME_ACTION, PAGES } from '@my-app/constants';
+import { BUTTON_THEME, GAME_ACTION, GAME_MAX_SCORE, PAGES } from '@my-app/constants';
 import { Menu, Menu__item } from '@/styled/Menu';
 
 import { PlayersGrid } from '@/components/PlayersGrid/PlayersGrid';
 import { Button } from '@/components/Button/Button';
+import { HeaderInGame } from '@/components/HeaderInGame/HeaderInGame';
 
 export const ScoresPage = () => {
   const history = useHistory();
   const players = useSelector((store: TStore) => store.partyReducer.players);
+  const rewards = useSelector((store: TStore) => store.partyReducer.rewards);
   const game_action = useSelector((store: TStore) => store.partyReducer.game_action);
   const game_master = useSelector((store: TStore) => store.partyReducer.game_master);
 
   useEffect(() => {
-    if (game_action !== GAME_ACTION.ALL_GUESS_DONE) {
+    if (game_action !== GAME_ACTION.ALL_GUESS_DONE && game_action !== GAME_ACTION.END_GAME) {
       history.replace(PAGES.GAME);
     }
   }, [game_action, history]);
@@ -30,13 +32,22 @@ export const ScoresPage = () => {
   return (
     <Menu>
       <Menu__item>
-        <PlayersGrid players={players} />
+        <HeaderInGame>
+          {game_action === GAME_ACTION.END_GAME && (
+            <h4>{`Игрок ${players.filter((item) => item.score >= GAME_MAX_SCORE)[0].nick_name} победил`}</h4>
+          )}
+        </HeaderInGame>
+      </Menu__item>
+      <Menu__item>
+        <PlayersGrid players={players} rewards={rewards} />
       </Menu__item>
       {game_master && (
         <Menu__item>
-          <Button callback={nextRound} theme={BUTTON_THEME.LIGHT} width="auto">
-            Next round
-          </Button>
+          {game_action !== GAME_ACTION.END_GAME && (
+            <Button callback={nextRound} theme={BUTTON_THEME.LIGHT} width="auto">
+              Next round
+            </Button>
+          )}
         </Menu__item>
       )}
     </Menu>
